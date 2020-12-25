c14205ccb7f2ccaa5ae5d97114ff642cebc04c39
Mohamed Amine Bounya <mobounya@student.1337.ma>
2020-12-25 18:26:11 +0100

    added file for creating table

---
+++ config/create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE members (
+    member_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    INDEX(email)) engine=INNODB;
