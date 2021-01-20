4846cbb9efbaa934a17b2eccf9f6909424bc09e2
Mohamed Amine Bounya <mobounya@student.1337.ma>
2021-01-20 11:18:20 +0100

    remove exercise folder

--- ex/config/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $DB_DSN = "mysql:host=localhost;port=3306;dbname=wa4e";
-    $DB_USER = "root";
-    $DB_PASSWORD = "HORSES";
-?>
--- ex/config/setup.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once("database.php");
-    $pdo = new pdo($DB_DSN, $DB_USER, $DB_PASSWORD);
-    $pdo->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
